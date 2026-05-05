e69481de0c0dee8b1ffbfd6d4633749c21c92070
Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>
2026-05-05 02:00:56 -0600

    Add predicate registry
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>

---
+++ pkg/slsa/eval/registry.go
@@ -0,0 +1,59 @@
+// SPDX-FileCopyrightText: Copyright 2026 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package eval
+
+import (
+	provenancev01 "github.com/in-toto/attestation/go/predicates/provenance/v01"
+	provenancev02 "github.com/in-toto/attestation/go/predicates/provenance/v02"
+	provenancev1 "github.com/in-toto/attestation/go/predicates/provenance/v1"
+	sourceprovenance "github.com/slsa-framework/source-tool/pkg/provenance"
+	"google.golang.org/protobuf/proto"
+)
+
+// SLSA predicate type URIs supported by the verifier.
+const (
+	PredicateProvenanceV01    = "https://slsa.dev/provenance/v0.1"
+	PredicateProvenanceV02    = "https://slsa.dev/provenance/v0.2"
+	PredicateProvenanceV1     = "https://slsa.dev/provenance/v1"
+	PredicateSourceProvenance = sourceprovenance.SourceProvPredicateType
+)
+
+// PredicateFactory returns an empty proto.Message of the matching predicate type.
+type PredicateFactory func() proto.Message
+
+// registeredPredicates maps predicate-type URIs to their proto factories.
+// The factories are used both to register descriptors with cel.Env and
+// (in later phases) to unmarshal predicate payloads.
+var registeredPredicates = map[string]PredicateFactory{
+	PredicateProvenanceV01:    func() proto.Message { return &provenancev01.Provenance{} },
+	PredicateProvenanceV02:    func() proto.Message { return &provenancev02.Provenance{} },
+	PredicateProvenanceV1:     func() proto.Message { return &provenancev1.Provenance{} },
+	PredicateSourceProvenance: func() proto.Message { return &sourceprovenance.SourceProvenancePred{} },
+}
+
+// IsKnownPredicateType reports whether the given URI matches a SLSA
+// predicate type the verifier supports.
+func IsKnownPredicateType(uri string) bool {
+	_, ok := registeredPredicates[uri]
+	return ok
+}
+
+// KnownPredicateTypes returns the list of supported predicate-type URIs.
+func KnownPredicateTypes() []string {
+	out := make([]string, 0, len(registeredPredicates))
+	for k := range registeredPredicates {
+		out = append(out, k)
+	}
+	return out
+}
+
+// NewPredicate returns an empty proto.Message for the given predicate type
+// or false if the type is unknown.
+func NewPredicate(predicateType string) (proto.Message, bool) {
+	f, ok := registeredPredicates[predicateType]
+	if !ok {
+		return nil, false
+	}
+	return f(), true
+}
